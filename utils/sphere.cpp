5da18c3a33443de50f1d2c83c78bd53e148f603c
demial003 <demial003@gmail.com>
2026-06-19 16:16:18 -0400

    almost done with sphere maybe

--- utils/sphere.cpp
+++ utils/sphere.cpp
@@ -27,29 +27,25 @@ void Sphere::genVertices(float *vboIdx, int *eboIdx) {
       *(basePtr + 1) = y;
       *(basePtr + 2) = z;
 
-      //      std::cout << vertNumber << ": " << x << ", " << y << ", " << z <<
-      //      "\n";
-      //
+      std::cout << vertNumber << ": " << x << ", " << y << ", " << z << "\n";
     }
   }
 
-  for (auto i = 0; i < slices; i++) {
-    unsigned int leftIdxOld = getVertexNumber(i, 0);
-    unsigned int rightIdxOld = getVertexNumber(i + 1, 1);
-    for (auto j = 0; j < stacks - 1; j++) {
-      unsigned int leftIdxNew = getVertexNumber(i, j + 1);
-      unsigned int rightIdxNew = getVertexNumber(i + 1, j + 2);
+  for (int i = 0; i < slices; i++) {
+    *(eboIdx++) = getVertexNumber(i, 0);
+    *(eboIdx++) = getVertexNumber(i + 1, 1);
+    *(eboIdx++) = getVertexNumber(i, 1);
+  }
 
-      *(eboIdx++) = leftIdxOld;
-      *(eboIdx++) = rightIdxOld;
-      *(eboIdx++) = leftIdxNew;
+  for (int i = 0; i < slices; i++) {
+    for (int j = 1; j < stacks - 1; j++) {
+      *(eboIdx++) = getVertexNumber(i, j);
+      *(eboIdx++) = getVertexNumber(i + 1, j);
+      *(eboIdx++) = getVertexNumber(i, j + 1);
 
-      *(eboIdx++) = leftIdxNew;
-      *(eboIdx++) = rightIdxNew;
-      *(eboIdx++) = rightIdxOld;
-
-      leftIdxOld = leftIdxNew;
-      rightIdxOld = rightIdxNew;
+      *(eboIdx++) = getVertexNumber(i, j + 1);
+      *(eboIdx++) = getVertexNumber(i + 1, j + 1);
+      *(eboIdx++) = getVertexNumber(i + 1, j);
     }
   }
 }
